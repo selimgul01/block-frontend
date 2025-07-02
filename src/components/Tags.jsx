91da1b7a55c3dc923645417df478a229f9bd852d
Selim GÜL <156907023+selimgul01@users.noreply.github.com>
2025-07-02 23:12:01 +0300

    Update Tags.jsx

--- src/components/Tags.jsx
+++ src/components/Tags.jsx
@@ -14,7 +14,7 @@ const Tags = () => {
   }
 
   return (
-    <div className=" flex xl:flex-col xl:w-[350px] justify-between border-b  xl:space-y-4 space-x-3 py-5 px-10 text-[#5f4ca6]  overflow-auto  bg-white rounded-xl">
+    <div className=" flex xl:flex-col xl:w-[350px] justify-between border-b  xl:space-y-4 space-y-3 py-5 px-10 text-[#5f4ca6]  overflow-auto  bg-white rounded-xl">
       {tags?.map((tag,i) => (
         <span key={i} onClick={()=>categoryHandler(tag)}  className="flex items-center cursor-pointer px-5 py-2 hover:scale-110 transition-all  rounded-lg border-b border-gray-400 font-semibold">
           <CiHashtag size={20} />
